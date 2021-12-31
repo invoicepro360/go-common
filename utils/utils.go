bb71a90436b11b61905565e7736284c0c800db79
Emad Najeeb <emad.najeeb@gmail.com>
2021-12-30 17:42:33 -0800

    Move route handlers back to individual apis

--- utils/utils.go
+++ utils/utils.go
@@ -2,12 +2,8 @@ package utils
 
 import (
 	"encoding/json"
-	"fmt"
-	"net"
 	"net/http"
-	"time"
 
-	"github.com/invoicepro360/go-common/config"
 	"github.com/invoicepro360/go-common/templates"
 )
 
@@ -75,51 +71,4 @@ func SuccessResponseResults(r *http.Request, w http.ResponseWriter, httpStatus i
 	// write response
 	json.NewEncoder(w).Encode(goodResponse)
 	return
-}
-
-// NoRouteFoundHandler handles cases where an undefined routes are requested
-func NoRouteFoundHandler(w http.ResponseWriter, r *http.Request) {
-	errorMessage := fmt.Sprintf("Invalid endpoint request (%v)", r.URL.Path)
-	FailedResponse(r, w, http.StatusNotFound, "", errorMessage)
-}
-
-// HealthCheckHandler handles /healthcheck route
-func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	var items []templates.HealthCheckItem
-	var item templates.HealthCheckItem
-	var hasError = false
-
-	// test container
-	item.Name = "CONTAINER"
-	item.IsHealthy = true
-	item.Message = "Container is connected"
-	items = append(items, item)
-
-	// test database connection
-	conn, err := net.DialTimeout("tcp", config.DBHost+":"+config.DBPort, 2*time.Second)
-	if err != nil {
-		item.Name = "DATABASE"
-		item.IsHealthy = false
-		item.Message = "Database connection failed"
-		items = append(items, item)
-		hasError = true
-	} else {
-		item.Name = "DATABASE"
-		item.IsHealthy = true
-		item.Message = "Database is connected"
-		items = append(items, item)
-		defer conn.Close()
-	}
-
-	// set headers
-	w.Header().Set("Content-Type", "application/json")
-	if hasError == false {
-		// success (200)
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(items)
-	} else {
-		// failed (503)
-		w.WriteHeader(http.StatusServiceUnavailable)
-		json.NewEncoder(w).Encode(items)
-	}
 }
